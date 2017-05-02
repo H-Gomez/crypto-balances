5c80c219952f0b57107d9d92cc61ccb18d8a6bb2
H-Gomez <hmgomez90@gmail.com>
2017-05-02 09:15:57 +0100

    Revised code for Google API

--- api/google.js
+++ api/google.js
@@ -1,19 +1,32 @@
+const fs = require('fs');
+const readline = require('readline');
 const google = require('googleapis');
 const googleAuth = require('google-auth-library');
-const config = require('~/lib/configuration');
 
-//load credentials from configuration file.
-let credentials = config.get('google:key');
-authorize(JSON.parse(credentials), listMajors);
+// Google sheets required variables.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/spreadsheets'];
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
+const TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+
+function PostToSheet(data) {
+    // Load client secrets from a local file.
+    fs.readFile('./config/client_secret.json', function processClientSecrets(error, content) {
+        if (error) {
+            console.log('Error loading client secret file: ' + error);
+            return;
+        }
+        // Authorize a client with the loaded credentials, then call the Google Sheets API.
+        authorize(JSON.parse(content), updateSheets, data);
+    });
+}
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
- *
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
+function authorize(credentials, callback, dataset) {
     let clientSecret = credentials.installed.client_secret;
     let clientId = credentials.installed.client_id;
     let redirectUrl = credentials.installed.redirect_uris[0];
@@ -21,29 +34,33 @@ function authorize(credentials, callback) {
     let oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
     // Check if we have previously stored a token.
-    getNewToken(oauth2Client, callback);
+    fs.readFile(TOKEN_PATH, function(err, token) {
+        if (err) {
+            getNewToken(oauth2Client, callback);
+        } else {
+            oauth2Client.credentials = JSON.parse(token);
+            callback(oauth2Client, dataset);
+        }
+    });
 }
 
 /**
  * Get and store new token after prompting for user authorization, and then
  * execute the given callback with the authorized OAuth2 client.
- *
  * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
+ * client.
  */
 function getNewToken(oauth2Client, callback) {
     let authUrl = oauth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPES
     });
-
     console.log('Authorize this app by visiting this url: ', authUrl);
     let rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
-
     rl.question('Enter the code from that page here: ', function(code) {
         rl.close();
         oauth2Client.getToken(code, function(err, token) {
@@ -58,27 +75,44 @@ function getNewToken(oauth2Client, callback) {
     });
 }
 
-function listMajors(auth) {
-    let sheets = google.sheets('v4');
-    sheets.spreadsheets.values.get({
-        auth: auth,
-        spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-        range: 'Class Data!A2:E',
-    }, function(err, response) {
-        if (err) {
-            console.log('The API returned an error: ' + err);
-            return;
+/**
+ * Store token to disk be used in later program executions.
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+    try {
+        fs.mkdirSync(TOKEN_DIR);
+    } catch (err) {
+        if (err.code != 'EEXIST') {
+            throw err;
         }
-        var rows = response.values;
-        if (rows.length == 0) {
-            console.log('No data found.');
+    }
+    fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+    console.log('Token stored to ' + TOKEN_PATH);
+}
+
+function updateSheets(auth, valueToPost) {
+    let sheets = google.sheets('v4');
+
+    sheets.spreadsheets.values.update({
+        auth: auth,
+        spreadsheetId: '1-MoC2Ph6Hi7DLFvsKJ-jXPlwWktWsqmL0Rv8qsUTESo',
+        range: 'BTC Trades!C10',
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+            range: 'BTC Trades!C10',
+            majorDimension: 'ROWS',
+            values: [[valueToPost]]
+        }
+    }, function(error, response) {
+        if (error) {
+            console.log("There was an error updating the sheet " + error);
         } else {
-            console.log('Name, Major:');
-            for (var i = 0; i < rows.length; i++) {
-                var row = rows[i];
-                // Print columns A and E, which correspond to indices 0 and 4.
-                console.log('%s, %s', row[0], row[4]);
-            }
+            console.log("update successful " + response);
         }
     });
 }
+
+module.exports = {
+    post: PostToSheet
+};
